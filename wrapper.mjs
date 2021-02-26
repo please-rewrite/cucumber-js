5332973c10faa625308ac315792002b772fcd196
davidgoss <david@davidgoss.co>
2021-02-26 12:30:53 +0000

    dont even need these bits?

--- wrapper.mjs
+++
@@ -1,6 +0,0 @@
-import cucumber from './lib/index.js'
-
-export const Given = cucumber.Given
-export const When = cucumber.When
-export const Then = cucumber.Then
-export const BeforeAll = cucumber.BeforeAll
